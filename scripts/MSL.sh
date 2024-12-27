3036ca16e18c6ba1017226891235bd6695405527
Ari Mogi <arimogi@yahoo.com>
2024-12-27 20:58:57 +0700

    Adjust the script  for train and testing.

--- scripts/MSL.sh
+++ scripts/MSL.sh
@@ -10,7 +10,16 @@ python main.py \
     --input_c 55 \
     --output_c 55
 
-#python main.py --anormly_ratio 1  --num_epochs 10      --batch_size 256     --mode test    --dataset MSL   --data_path dataset/MSL  --input_c 55    --output_c 55  --pretrained_model 20
+# python main.py \
+#     --anormly_ratio 1 \
+#     --num_epochs 10 \
+#     --batch_size 256 \
+#     --mode test \
+#     --dataset MSL \
+#     --data_path dataset/MSL \
+#     --input_c 55 \
+#     --output_c 55 \
+#     --pretrained_model 20
 
 
 
